8579d870efd4e4d305e47a2610b55d65deddd19e
Lain Carvalho <lain.cesar@gmail.com>
2014-05-19 09:06:02 -0300

    fui

--- prob.py
+++ prob.py
@@ -1,38 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from random import shuffle
 
-#import random
-
-#PESO = (1, 5, 10, 25, 50, 100)
-#ESTOQUE = (20, 10, 15, 20, 10)
-#PESO = (80, 16, 40, 26, 50)
-#ESTOQUE = (4, 10, 15, 20, 10)
-PESO = [2, 6, 10]
-ESTOQUE = [15, 20, 10]
-NOMES = "ABCI"
-CRITERIO = [0, 1, 2]
-META = 101
-
-# TODO : botar nomes conforme nos slides.
 
 class Problem(object):
-    """docstring for problem"""
-# TODO botar docstrings
-    def __init__(self, meta, criterio, peso, estoque):
+    """docstring for ClassName"""
+    def __init__(self, meta, criterio, peso, estoque, tipo):
         self.meta = meta
         self.peso = [0] + peso
-        self.inicial = No(0, 0, None, 0)
-        self.estoque = [1] + estoque
-        self.criterio = [0] + [ x + 1 for x in criterio ]  # criterio para ordem de visita de nó filho
+        self.inicial = No(0, 1, None, 0)
+        self.estoque = [0] + estoque
+        self.criterio = [0] + [x + 1 for x in criterio]  # criterio para ordem de visita de nó filho
         self.criterio.append(None)
+        self.memo = [meta+1] * meta
         self.melhor = self.inicial
         self.melhor_precisao = 0
-
-        # TODO variaveis globais?
+        self.tipo = tipo
 
     def trata_objetivo(self, no, peso_total):
-        """Atualiza o melhor candidato."""
+        """Atualiza o melhor candidato. Retorna True se é pra finalizar."""
         nova_precisao = peso_total  # quão perto chegou da capacidade
+        if (peso_total == self.meta) and (self.tipo == "a*"):
+            return True
         if nova_precisao > self.melhor_precisao:
             self.melhor = no  # medida de desempenho primária é se chega no alvo
             self.melhor_precisao = nova_precisao
@@ -40,16 +29,19 @@ class Problem(object):
             if no.custo < self.melhor.custo: # segundo objetivo é o custo em passos
                 self.melhor = no
                 self.melhor_precisao = nova_precisao
+        return False
 
     def solucao(self):
         no = self.melhor
-        acoes = []
+        acoes = [0] * (len(self.criterio) - 1)
         peso = self.melhor_precisao
-        resposta = "O melhor caminho teve custo {}, o peso calculado para coletar foi de {}".format(no.custo, peso)
+        resposta = "O melhor caminho teve custo {}, o peso calculado para se coletar foi de {}".format(no.custo, peso)
+
         while no != self.inicial:
-            acoes.append("pegar item {}".format(no.estado))
+            acoes[no.estado] += 1
             no = no.pai
-        return "{}. A sequencia que leva ao objetivo é: {}.".format(resposta, ", ".join(acoes))
+        acoes = ["{} de {} ({} kilos)".format(n, i, self.peso[i]) for (i, n) in enumerate(acoes) if n > 0]
+        return "{}. A sequencia do objetivo é: pegar {}.".format(resposta, ", ".join(acoes))
 
     def __str__(self):
         val_vars = [
@@ -71,31 +63,35 @@ class No(object):
         self.prox = link
 
     def __str__(self):
-        return "{} -> {}".format(self.estado, self.prox)
+        return "{}".format(self.estado)
 
 
 def dfs(problema):
     """Retorna uma solução de menor custo, dado um Problema problema"""
     peso = problema.peso
     criterio = problema.criterio
+    memo = problema.memo
     pilha = [problema.inicial]
     peso_atual = 0
-    print problema
 
     while pilha != []:
         atual = pilha[-1]
-        #print "no", atual.estado
         if criterio[atual.prox] is None:  # nó atual foi explorado por completo
             if atual.eh_folha:
-                problema.trata_objetivo(atual, peso_atual)
+                if (problema.trata_objetivo(atual, peso_atual)):
+                    break
                 # atual é nó folha, nó interno não precisa tratar, nunca é ótimo
             peso_atual -= peso[atual.estado]
-            problema.estoque[atual.estado] += 1
-            del pilha[-1]
+            if atual.estado != 0:
+                problema.estoque[atual.estado] += 1
+            pilha.pop()
         else:
-            while (criterio[atual.prox] is not None) and \
-                  (peso_atual + peso[criterio[atual.prox]] > problema.meta):
+            while (criterio[atual.prox] is not None):
+                prox_peso = peso_atual + peso[criterio[atual.prox]]
+                if (prox_peso < problema.meta) and (atual.custo + 1 < memo[prox_peso]):
+                    break
                 atual.prox += 1 # pega o arco/link pro próximo filho válido
+
             # atual.prox agora é o proximo filho segundo CRITERIO ou acabou os filhos
             if criterio[atual.prox] is not None:
                 item = criterio[atual.prox]
@@ -103,19 +99,40 @@ def dfs(problema):
                 novo = No(item, atual.prox, atual, atual.custo + 1)
                 atual.prox += 1
                 peso_atual += peso[item]
+                if novo.custo < memo[peso_atual]:
+                    memo[peso_atual] = novo.custo
                 problema.estoque[item] -= 1
                 if problema.estoque[item] == 0:
-                    novo.prox += 1  # o próximo item a pegar é o mesmo do anterior a não ser que acabe o estoque
+                    novo.prox += 1  # o próximo item é o mesmo do anterior a menos que acabe o estoque
                 pilha.append(novo)
                 #print atual
 
-def busca_em_profundidade():
-    pass
+def busca_em_profundidade(meta, criterio, peso, estoque):
+    criterio .shuffle()
+    problema = Problem(meta, criterio, peso, estoque, "profundidade")
+    dfs(problema)
+    return problema.solucao()
+
+def busca_a_asterisco(meta, criterio, peso, estoque):
+    criterio = sorted([(i, v) for i, v in enumerate(peso)], key=lambda x: x[1])
+    criterio = reversed([x[0] for x in criterio])
+    problema = Problem(META, criterio, PESO, ESTOQUE, "a*")
+    dfs(problema)
+    return problema.solucao()
+
+META = 37
+ESTOQUE = [100, 110, 100, 110, 150, 130]
+PESO = [10, 7, 2, 13, 11, 26]
+NOME = "IABCDEF"
+CRITERIO = [1, 0, 2, 4, 3, 5]
 
 if __name__ == "__main__":
-    problema = Problem(META, CRITERIO, PESO, ESTOQUE)
-    dfs(problema)
-    print "capacidade do robô de {} kilos, criterio {}".format(META, ", ".join(map(str, CRITERIO)))
-    print "objetos para levar são esses:\n(codigo, peso, estoque)"
+    print "capacidade do robô de %d kilos" % META
+    print "objetos para levar são esses:\n\n(codigo, peso, estoque)"
     print "\n".join(" ".join([str(x), str(y), str(z)]) for x, y, z in zip(xrange(len(ESTOQUE)), PESO, ESTOQUE))
-    print problema.solucao()
+    print "================="
+    print "pela busca em profundidade:"
+    print busca_em_profundidade(META, CRITERIO, PESO, ESTOQUE)
+    print "================="
+    print "Pela busca A*:"
+    print busca_a_asterisco(META, CRITERIO, PESO, ESTOQUE)
